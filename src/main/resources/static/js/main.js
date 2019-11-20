6e33adfa9b7d92169e5e4f67afcb93cdc4a9a086
Eric Couch <james.couch@ruminatetech.com>
2019-11-19 20:06:56 -0600

    added cars.html and js

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,51 @@
+function postData(url, data) {
+    return fetch(url, {
+        body: JSON.stringify(data),
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "user-agent": "Mozilla/4.0 MDN Example",
+            "content-type": "application/json"
+        },
+        method: "POST",
+        mode: "same-origin",
+        redirect: "follow",
+        referrer: "no-referrer"
+    }).then(reponse => response.json());
+}
+
+function getData(url, data) {
+    // Default options are marked with *
+    return fetch(url, {
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, same-origin, *omit
+        headers: {
+            "user-agent": "Mozilla/4.0 MDN Example",
+            "content-type": "application/json"
+        },
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "same-origin", // no-cors, cors, *same-origin
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer" // *client, no-referrer
+    }).then(response => response.json()); // parses response to JSON
+}
+
+function updateCars(d) {
+    let carDiv = document.getElementById("cars");
+    carDiv.innerHTML = '';
+    d.forEach(function(car) {
+        carDiv.innerHTML += 'Year: ' + car.year + '<br>';
+        carDiv.innerHTML += 'Make: ' + car.make + '<br>';
+        carDiv.innerHTML += "Model: " + car.model + '<br>'
+        carDiv.innerHTML += '<hr>';
+    })
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let make = document.getElementById("make");
+    make.addEventListener("change", function() {
+        let cars = getData('/auto/' + this.value).then(data => updateCars(data));;
+
+    })
+})
